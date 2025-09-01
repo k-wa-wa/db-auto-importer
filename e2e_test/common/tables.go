6d74329e12bbe7899410d770a8610ef4d4e63629
watanabe <sek.ohei.w0822@icloud.com>
2025-09-01 20:36:57 +0900

    vibe: 全てのテーブルに対する構造体を定義

--- e2e_test/common/tables.go
+++ e2e_test/common/tables.go
@@ -1,6 +1,34 @@
 package common
 
+import "time"
+
 type User struct {
+	ID        int       `db:"id"`
+	Name      string    `db:"name"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type Post struct {
+	ID        int    `db:"id"`
+	Title     string `db:"title"`
+	Content   string `db:"content"`
+	UserID    int    `db:"user_id"`
+	Published bool   `db:"published"`
+}
+
+type Product struct {
+	ID    int     `db:"id"`
+	Name  string  `db:"name"`
+	Price float64 `db:"price"`
+}
+
+type Tag struct {
 	ID   int    `db:"id"`
 	Name string `db:"name"`
+}
+
+type ProductTag struct {
+	ProductID int       `db:"product_id"`
+	TagID     int       `db:"tag_id"`
+	CreatedAt time.Time `db:"created_at"`
 }
